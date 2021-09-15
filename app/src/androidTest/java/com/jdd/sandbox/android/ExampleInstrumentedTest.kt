0537d44157d7677b505fe4d1392b8e9596cc4791
jondoetsch27 <jd@enfuse.io>
2021-09-14 18:25:55 -0700

    09/14 - successful second button test @jondoetsch27

--- app/src/androidTest/java/com/jdd/sandbox/android/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jdd/sandbox/android/ExampleInstrumentedTest.kt
@@ -1,5 +1,12 @@
 package com.jdd.sandbox.android
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -20,5 +27,17 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.jdd.sandbox.android", appContext.packageName)
+    }
+
+    @Test
+    fun testFirstButton() {
+        val mainScreen = launchFragmentInContainer<FirstFragment>()
+        onView(withId(R.id.button_first)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testSecondButton() {
+        val mainScreen = launchFragmentInContainer<FirstFragment>()
+        onView(withId(R.id.button_second)).check(matches(isDisplayed()))
     }
 }
